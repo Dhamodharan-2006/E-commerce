bd5f535ff52403a97253af28ba92969843c7731d
Dhamodharan <d2341717@gmail.com>
2026-05-24 20:12:52 +0530

    create superuser on deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,13 @@ pip install -r backend/requirements.txt
 cd backend
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+cd backend && python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
