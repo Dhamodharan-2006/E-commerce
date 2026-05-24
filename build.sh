6fd8444126a2020ce9ab8c18e9a3dc06d797f6f6
Dhamodharan <d2341717@gmail.com>
2026-05-24 20:16:15 +0530

    fix superuser creation script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd backend
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-cd backend && python manage.py shell -c "
+python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
