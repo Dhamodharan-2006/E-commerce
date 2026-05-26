a5a6f169a9ad2bd956908157921b7736833b45a9
Dhamodharan <d2341717@gmail.com>
2026-05-26 17:26:33 +0530

    update otp

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,14 +76,12 @@ TEMPLATES = [
         },
     },
 ]
-
-# Email config
-EMAIL_HOST = os.environ.get("EMAIL_HOST")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS") == "True"
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
